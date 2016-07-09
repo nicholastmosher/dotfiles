14643aeae68a8b6e0dca6b754852650542837048
Nick Mosher <nicholastmosher@gmail.com>
2016-07-09 02:06:56 -0400

    Updates to install.

--- .vim/setup.sh
+++ .vim/setup.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 OLD=`pwd` DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"; cd $DIR
 git clone -q https://github.com/gmarik/vundle.git $HOME/.vim/bundle/vundle
 vim -u .vimrc_setup
--- install.sh
+++ install.sh
@@ -5,23 +5,29 @@ case $yn in
 	* ) exit;;
 esac
 
-GIT=`which git`
-
-# Download dotfiles into ~/.dot
-git clone -q --bare https://github.com/nicholastmosher/dotfiles.git $HOME/.dot
-
 # Alias "dot" as a special git command with repo and work tree paths.
+GIT=`which git`
 export DOT="$GIT --git-dir=$HOME/.dot --work-tree=$HOME"
 alias dot="$DOT"
 
-# Force checkout master, writing files from repo into home.
-dot checkout -f master
+# Download dotfiles into ~/.dot
+if [ -d $HOME/.dot ]; then
+	# Force update from origin.
+	dot fetch -q origin
+	dot reset -q --hard origin/master
+elsif
+	# Clone and force checkout.
+	git clone -q --bare https://github.com/nicholastmosher/dotfiles.git $HOME/.dot
+	dot checkout -q -f master
+fi
 
 # Download oh-my-zsh into ~/.oh-my-zsh
-git clone -q https://github.com/nicholastmosher/oh-my-zsh.git $HOME/.oh-my-zsh
+if [ ! -d $HOME/.oh-my-zsh ]; then
+	git clone -q https://github.com/nicholastmosher/oh-my-zsh.git $HOME/.oh-my-zsh
+fi
 
 # Setup vim.
-$HOME/.vim/setup.sh > /dev/null
+$HOME/.vim/setup.sh &> /dev/null
 
 # Finished
-echo "Install complete!"
+echo "\nInstall complete!"
