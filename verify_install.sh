80609053c9afb2022843d1d00d1970fd23597e2d
Nick Mosher <nicholastmosher@gmail.com>
2017-09-11 23:30:42 -0400

    Debugging verify_install

--- verify_install.sh
+++ verify_install.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
+set -x
+
 [[ -x "$(command -v git)" ]] || exit 1
+echo "GIT"
+echo $(which git)
 [[ -x "$(command -v vim)" ]] || exit 1
+echo "VIM"
+echo $(which vim)
 [[ -x "$(command -v zsh)" ]] || exit 1
+echo "ZSH"
+echo $(which zsh)
 [[ -x "$(command -v rg)"  ]] || exit 1
+echo "RG"
+echo $(which rg)
+
+set +x
 
 exit 0
