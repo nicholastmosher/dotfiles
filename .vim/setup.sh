babe58315ea8195fef48a5b29492f2a06b1e90d4
Nick Mosher <nicholastmosher@gmail.com>
2021-01-31 13:15:11 -0500

    Get rid of vim configs

--- .vim/setup.sh
+++
@@ -1,5 +0,0 @@
-OLD=`pwd` DIR="$(cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd)"; cd $DIR
-echo "Cloning vundle at $DIR..."
-git clone -q https://github.com/gmarik/vundle.git $HOME/.vim/bundle/vundle
-echo | echo | vim -u .vimrc_setup
-cd $OLD
