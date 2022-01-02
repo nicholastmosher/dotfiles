e0595e18a8aed681838aaa57f27ca459333779f5
Nick Mosher <nicholastmosher@gmail.com>
2022-01-02 14:55:09 -0500

    Update nix config

--- nix/apply-user.sh
+++ nix/apply-user.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 pushd ~/.dotfiles/nix
 nix build .#homeManagerConfigurations.nick.activationPackage
-sudo ./result/activate
+./result/activate
 popd
--- nix/flake.nix
+++ nix/flake.nix
@@ -2,8 +2,8 @@
   description = "NixOS Configurations";
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-20.09";
-    home-manager.url = "github:nix-community/home-manager/release-20.09";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
