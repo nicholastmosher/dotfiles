6ae64b495018d41be85854d27986b8a977f4d993
Nick Mosher <nicholastmosher@gmail.com>
2022-01-02 15:24:35 -0500

    Use nix 21.11

--- nix/flake.nix
+++ nix/flake.nix
@@ -2,8 +2,8 @@
   description = "NixOS Configurations";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    home-manager.url = "github:nix-community/home-manager";
+    nixpkgs.url = "nixpkgs/nixos-21.11";
+    home-manager.url = "github:nix-community/home-manager/release-21.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
