3071e50960277d9542713412a54500385ec58737
Nick Mosher <nicholastmosher@gmail.com>
2022-03-12 14:19:34 -0500

    Update kakoune config

--- nix/users/common.nix
+++ nix/users/common.nix
@@ -165,7 +165,22 @@
     };
   };
 
-  programs.kakoune.enable = true;
+  programs.kakoune = {
+    enable = true;
+    config = {
+      colorScheme = "tomorrow-night";
+      numberLines.enable = true;
+      tabStop = 4;
+      keyMappings = [
+        {
+          key = "<tab>";
+          mode = "insert";
+          effect = "    ";
+          docstring = "Use spaces instead of tabs";
+        }
+      ];
+    };
+  };
 
   programs.alacritty.enable = true;
   programs.alacritty.settings = {
