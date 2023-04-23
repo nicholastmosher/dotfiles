0faf6c8c350b229e63bcb7d59328f8e0033a6235
Nick Mosher <nicholastmosher@gmail.com>
2023-04-23 05:22:56 -0400

    aan: Remove miette in favor of color-eyre

--- aan/src/bin/main.rs
+++ aan/src/bin/main.rs
@@ -1,22 +1,12 @@
-use an::cli::Cli;
-use clap::{CommandFactory, FromArgMatches, Parser};
-use miette::IntoDiagnostic;
+use aan::cli::Cli;
+use clap::{CommandFactory, FromArgMatches};
 use tracing_subscriber::prelude::*;
 use tracing_subscriber::Registry;
 
 #[tokio::main]
-async fn main() -> miette::Result<()> {
-    let _ = dotenv::dotenv();
-    miette::set_hook(Box::new(|_| {
-        Box::new(
-            miette::MietteHandlerOpts::new()
-                .terminal_links(true)
-                .unicode(false)
-                .context_lines(3)
-                .tab_width(4)
-                .build(),
-        )
-    }))?;
+async fn main() -> color_eyre::Result<()> {
+    color_eyre::install()?;
+    dotenv::dotenv().ok();
 
     if let None | Some("") = std::env::var("RUST_LOG").as_deref().ok() {
         std::env::set_var("RUST_LOG", "info");
@@ -25,14 +15,13 @@ async fn main() -> miette::Result<()> {
     Registry::default()
         .with(tracing_subscriber::fmt::layer())
         .with(tracing_subscriber::EnvFilter::from_default_env())
-        .try_init()
-        .into_diagnostic()?;
+        .try_init()?;
     tracing::debug!("Initialized tracing!");
 
-    let clap_args = an::preprocessed_args();
+    let clap_args = aan::preprocessed_args();
     let command = Cli::command();
     let matches = command.get_matches_from(clap_args);
-    let cli = Cli::from_arg_matches(&matches).into_diagnostic()?;
+    let cli = Cli::from_arg_matches(&matches)?;
     cli.run().await?;
     Ok(())
 }
--- aan/src/error.rs
+++ aan/src/error.rs
@@ -1,39 +1,30 @@
-use miette::Diagnostic;
 use thiserror::Error;
 
 pub type Result<T, E = Error> = core::result::Result<T, E>;
 
-#[derive(Debug, Error, Diagnostic)]
+#[derive(Debug, Error)]
 pub enum Error {
     #[error("Shell error")]
-    #[diagnostic(code(xshell))]
     Shell(#[from] xshell::Error),
 
     #[error("Watchexec critical error")]
-    #[diagnostic(code(watchexec::critical))]
     WatchexecCritical(#[from] watchexec::error::CriticalError),
 
     #[error("Watchexec reconfiguration error")]
-    #[diagnostic(code(watchexec::config))]
     WatchexecReconfig(#[from] watchexec::error::ReconfigError),
 
     #[error("Tokio watchexec error")]
-    #[diagnostic(code(watchexec::tokio))]
     Tokio(#[from] tokio::task::JoinError),
 
     #[error("Flume rx error")]
-    #[diagnostic(code(flume::rx))]
     FlumeRx(#[from] flume::RecvError),
 
     #[error("Failed to find git repo")]
-    #[diagnostic(code(git::discover))]
     GitDiscovery(#[from] git_repository::discover::Error),
 
     #[error("Clap error")]
-    #[diagnostic(code(clap))]
     Clap(#[from] clap::Error),
 
     #[error(transparent)]
-    #[diagnostic(code(std::io))]
     Io(#[from] std::io::Error),
 }
