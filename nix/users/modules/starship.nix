e633fb4e3f6f61a409532ce3906c71013c1c2d32
Nick Mosher <nicholastmosher@gmail.com>
2023-04-23 05:19:38 -0400

    Tweak starship

--- nix/users/modules/starship.nix
+++ nix/users/modules/starship.nix
@@ -5,6 +5,9 @@
     settings = {
       aws.disabled = true;
       nix_shell.symbol = "❄️ ";
+      git_status.disabled = true;
+      directory.truncate_to_repo = false;
+      directory.truncation_length = 10;
     };
   };
 }
