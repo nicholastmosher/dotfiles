7f4adc17410f7e191c9a92a619d24f64f8906a01
Nick Mosher <nicholastmosher@gmail.com>
2023-05-01 10:04:56 -0400

    Disable packages/rust/ruby starship modules

--- nix/users/modules/starship.nix
+++ nix/users/modules/starship.nix
@@ -5,6 +5,9 @@
     settings = {
       aws.disabled = true;
       nix_shell.symbol = "❄️ ";
+      package.disabled = true;
+      rust.disabled = true;
+      ruby.disabled = true;
       git_status.disabled = true;
       directory.truncate_to_repo = false;
       directory.truncation_length = 10;
