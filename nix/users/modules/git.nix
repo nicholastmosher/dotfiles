3145d3cb7fed0e5f351dee68abc514fb0ff6e3d1
Nick Mosher <nicholastmosher@gmail.com>
2023-07-13 14:55:55 -0400

    Fix git delta styling

--- nix/users/modules/git.nix
+++ nix/users/modules/git.nix
@@ -9,6 +9,13 @@
       options = {
         side-by-side = true;
         plus-style = ''syntax "#003700"'';
+        plus-emph-style = ''syntax "#008700"'';
+        features = "decorations";
+        decorations = {
+          file-style = "bold yellow ul";
+          file-decoration-style = "none";
+          hunk-header-style = "omit";
+        };
       };
     };
   };
